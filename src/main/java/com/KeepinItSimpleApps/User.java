bf50dad49dcb54ee84287a322a541a09798511c2
Andrew Donley <andrewjdonley@gmail.com>
2013-09-25 13:09:15 -0700

    Compiling Maven Velocity Website. Probably need to change the web.xml in order to get it to run with an index.vm

---
+++ src/main/java/com/KeepinItSimpleApps/User.java
@@ -0,0 +1,15 @@
+package com.KeepinItSimpleApps;
+
+public class User {
+
+    private String name;
+    private String address;
+
+    public User() { }
+
+    public User(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+    
+}
